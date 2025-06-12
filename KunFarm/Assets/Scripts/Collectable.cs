112521df79ed63d0297b5a240a283164dbb01532
haocp <haocpse184107@fpt.edu.vn>
2025-06-12 10:25:16 +0700

    add collect item vipprlo

---
+++ KunFarm/Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public CollectableType type;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        Player player = collision.GetComponent<Player>();
+        if (player)
+        {
+            player.inventory.Add(type);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
+
+public enum CollectableType
+{
+    NONE, EGG
+}
