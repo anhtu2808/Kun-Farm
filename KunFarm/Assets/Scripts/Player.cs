2c22ad13ee7c173d44ed55aef69ff718257a4a47
Uông Đình Dũng <dungminhhao@gmail.com>
2025-06-16 19:55:45 +0700

    adjust controller for digging

--- KunFarm/Assets/Scripts/Player.cs
+++ KunFarm/Assets/Scripts/Player.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Inventory inventory;
+    private TileManager tileManager;
 
     private void Awake()
     {
         inventory = new Inventory(21);
     }
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        if (tileManager != null)
+    //        {
+    //            Vector3Int position = new Vector3Int((int)transform.position.x, (int)transform.position.y, 0);
+
+    //            string tileName = tileManager.GetTileName(position);
+
+    //            if (!string.IsNullOrWhiteSpace(tileName))
+    //            {
+    //                if (tileName == "interactable" && inventory.toolbar.selectedSlot.itemName == "Hoe")
+    //                {
+    //                    tileManager.SetInteracted(position);
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }
