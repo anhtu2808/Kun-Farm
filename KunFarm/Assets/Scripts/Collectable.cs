b7c199acae16a2e89a9385921840367b18c7fa1a
Longnee <longthse184113@fpt.edu.vn>
2025-06-19 10:12:25 +0700

    ChickenPlusDeTrungThanhCOnGa

--- KunFarm/Assets/Scripts/Collectable.cs
+++ KunFarm/Assets/Scripts/Collectable.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
     public CollectableType type;
+    private GameObject chickenPrefab;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = 5;
+        if (type == CollectableType.EGG)
+        {
+            // Load prefab gà
+            chickenPrefab = Resources.Load<GameObject>("Prelabs/Chicken");
+
+            if (chickenPrefab == null)
+            {
+                Debug.LogWarning("Không tìm thấy prefab 'Prelabs/Chicken' trong Resources!");
+            }
+            else
+            {
+                StartCoroutine(HatchEgg());
+            }
+        }
+    }
+
+    private IEnumerator HatchEgg()
+    {
+        yield return new WaitForSeconds(10f); // 2 phút
+
+        if (chickenPrefab != null)
+        {
+            Instantiate(chickenPrefab, transform.position, Quaternion.identity);
+        }
+
+        Destroy(this.gameObject);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-
         Player player = collision.GetComponent<Player>();
         if (player)
         {
@@ -18,7 +46,6 @@ public class Collectable : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-
 }
 
 public enum CollectableType
