e0af2e27d20f52ee3e810d1795c027bf8b734e97
Longnee <longthse184113@fpt.edu.vn>
2025-07-08 12:44:45 +0700

    watering sound added into ainamtion

---
+++ KunFarm/Assets/Scripts/WateringAudio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class WateringAudio : MonoBehaviour
+{
+    // Kéo file âm thanh bạn muốn phát vào đây từ cửa sổ Project
+    public AudioClip wateringSound;
+
+    // Component để phát âm thanh
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Lấy component AudioSource, nếu chưa có thì thêm một cái mới
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    // Đây là hàm chúng ta sẽ gọi từ Animation Event
+    public void PlayWateringSound()
+    {
+        if (wateringSound != null)
+        {
+            // PlayOneShot cho phép phát âm thanh mà không làm gián đoạn âm thanh khác
+            audioSource.PlayOneShot(wateringSound);
+        }
+    }
+}
